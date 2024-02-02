0389300fdb8f737360891bab313a4ea7aa2b16c2
ghanatava <ghanatva@gmail.com>
2024-02-02 13:19:34 +0530

    custom server struct

--- main.go
+++ main.go
@@ -3,13 +3,29 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
-func HelloWorld(w http.ResponseWriter,r *http.Request){
-	fmt.Fprint(w,"Hello world")
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		fmt.Fprint(w, "Hello World")
+	case "/ninja":
+		fmt.Fprint(w, "Ninja")
+	default:
+		fmt.Fprint(w, "404 Page not Found")
+	}
+	fmt.Printf("%s %s\n", r.Method, r.URL.Path)
 }
 
-func main(){
-	http.HandleFunc("/",HelloWorld)
-	http.ListenAndServe("localhost:8080",nil)
+func main() {
+	http.HandleFunc("/", HelloWorld)
+	// http.ListenAndServe("localhost:8080", nil)
+	server := http.Server{
+		Addr:         "localhost:8080",
+		Handler:      nil,
+		ReadTimeout:  time.Second * 30,
+		WriteTimeout: time.Second * 30,
+	}
+	server.ListenAndServe()
 }
